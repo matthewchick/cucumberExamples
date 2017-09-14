eed5725beed8d7f8af274af16297d4bc12060a30
Matthew Chick <chikmatthew@gmail.com>
2017-09-14 20:47:28 +1000

    Create LoginStep by alt-return based on scenario and scenario outline

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+
+/*
+executeautomation.com
+Cucumber and Specflow are BDD
+Continuous integration - Jenkins 2.0 and TFS
+Automation testing tools - Selenium, Visual Studio Coded UI
+- https://www.youtube.com/playlist?list=PL6tu16kXT9Pp3NFZgLbPZXEykeGQwxGSx - C#
+- https://www.youtube.com/playlist?list=PL6tu16kXT9Pp6XV3L3lrWideBW6Mcwaa5 - Typescript
+https://mvnrepository.com/artifact/info.cukes/cucumber-java/1.2.5
+Use different approach called testRunner with RunWithattribute inside command cli
+work with multiple data using DataTable
+Highlight the statement and Use alt + enter to go to LoginStep
+Use Scenario Outline
+Working with Hooks
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
+
---
+++ src/test/java/Steps/LoginStep.java
@@ -0,0 +1,75 @@
+package Steps;
+
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/*
+  In Java, there are two hooks @Before and @After
+ */
+public class LoginStep {
+    @Given("^I navigate to the login page$")
+    public void iNavigateToTheLoginPage() throws Throwable {
+        System.out.println("I navigate to the login page");
+        System.out.println();
+    }
+    /* And I enter the username as "admin" and password as "adminpassword" inside Login.feature
+    @And("^I enter the username as \"([^\"]*)\" and password as \"([^\"]*)\"$")
+    public void iEnterTheUsernameAsAndPasswordAs(String username, String password) throws Throwable {
+        System.out.println("The Username is " + username + " ,password is " + password);
+    }
+    */
+    // Working with multiple data
+    @And("^I enter the following for login$")
+    public void iEnterTheFollowingForLogin(DataTable table) throws Throwable {
+        List<List<String>> data = table.raw();
+        System.out.println("The value is " + data.get(0).get(0).toString());
+        System.out.println("The value is " + data.get(0).get(1).toString());
+        /*
+        List<User> users = new ArrayList<User>();
+        users = table.asList(User.class);
+        for (User user: users){
+            System.out.println("The UsersName is " + user.username);
+            System.out.println("The Password is " + user.password);
+        } */
+        Map<String, String> mapacol;
+        mapacol = table.asMap(String.class, String.class);
+        System.out.println("Username: " + mapacol.get("username") + "  Password: " + mapacol.get("password") );
+
+    }
+
+    @And("^I enter ([^\"]*) and ([^\"]*)$")
+    public void iEnterUsernameAndPassword(String Username, String Password) throws Throwable {
+        System.out.println("Username is : " + Username);
+        System.out.println("Password is : " + Password);
+    }
+
+    @And("^I click login button$")
+    public void iClickLoginButton() throws Throwable {
+        System.out.println("I click login button");
+
+    }
+
+    @Then("^I should see the userform page$")
+    public void iShouldSeeTheUserformPage() throws Throwable {
+        System.out.println("I should see the userform page");
+
+    }
+
+    /*
+    public class User {
+        public String username;
+        public String password;
+
+        public User(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+    } */
+}
