f8e129e26c2b331cf94f18a91bbde6f12145a8ce
Matthew Chick <chikmatthew@gmail.com>
2017-09-14 21:03:09 +1000

    Add Hook

--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -1,4 +1,20 @@
 package Steps;
 
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
 public class Hook {
+    @Before
+    public void InitializeTest() {
+        System.out.println("Opening the browser : MOCK");
+    }
+    @After
+    public void TearDownTest(Scenario scenario) {
+        if (scenario.isFailed()){
+            //Take screenshot
+            System.out.println(scenario.getName());
+        }
+        System.out.println("Closing the browser : MOCK");
+    }
 }
